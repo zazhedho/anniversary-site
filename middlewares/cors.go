ff980e58e6e5548176166ee8b53cb7d70290fa49
zaiduszh <go.programmerz@gmail.com>
2026-02-27 10:35:01 +0700

    fixing issue cors

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -4,9 +4,21 @@ import "github.com/gin-gonic/gin"
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, Access-Control-Allow-Origin, Cache-Control")
+		origin := c.GetHeader("Origin")
+		if origin != "" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Vary", "Origin")
+		} else {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		}
+
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+
+		allowHeaders := "Content-Type, Authorization, Accept, Origin, Cache-Control, X-Requested-With, X-Setup-Token"
+		if requestedHeaders := c.GetHeader("Access-Control-Request-Headers"); requestedHeaders != "" {
+			allowHeaders = allowHeaders + ", " + requestedHeaders
+		}
+		c.Writer.Header().Set("Access-Control-Allow-Headers", allowHeaders)
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
